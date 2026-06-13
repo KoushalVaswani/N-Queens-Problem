7ecb9c2338b93b092a078804d10ae28a943c892f
KOUSHAL VASWANI <koushalvaswani3@gmail.com>
2026-06-13 20:27:54 +0530

    Add files via upload

---
+++ n_queens.py
@@ -0,0 +1,48 @@
+def solveNQueens(n):
+    board = [['.' for _ in range(n)] for _ in range(n)]
+    result = []
+
+    def isSafe(row, col):
+        for i in range(row):
+            if board[i][col] == 'Q':
+                return False
+
+        i, j = row - 1, col - 1
+        while i >= 0 and j >= 0:
+            if board[i][j] == 'Q':
+                return False
+            i -= 1
+            j -= 1
+
+        i, j = row - 1, col + 1
+        while i >= 0 and j < n:
+            if board[i][j] == 'Q':
+                return False
+            i -= 1
+            j += 1
+
+        return True
+
+    def backtrack(row):
+        if row == n:
+            result.append([''.join(r) for r in board])
+            return
+
+        for col in range(n):
+            if isSafe(row, col):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
+
+    backtrack(0)
+    return result
+
+
+n = 4
+solutions = solveNQueens(n)
+
+for i, solution in enumerate(solutions, 1):
+    print(f"Solution {i}:")
+    for row in solution:
+        print(row)
+    print()
